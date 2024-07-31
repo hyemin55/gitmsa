7f4f4ac5638accae2f26e326afd16026df436dfe
Pa-fs <whgpals641@naver.com>
2024-07-31 18:10:19 +0900

    240731

--- sql_work/240729_ex01.sql
+++ sql_work/240729_ex01.sql
@@ -31,7 +31,14 @@ where name = '박지성' and orders.custid = customer.custid and book.bookid = o
 -- 7 --
 select bookname
 from book, orders, customer
-where name != '박지성' and orders.custid = customer.custid and book.bookid = orders.bookid;
+where customer.name <> '박지성' and orders.custid = customer.custid and book.bookid = orders.bookid;
+
+select bookid, bookname
+from book
+where book.bookid in 
+(select bookid
+from orders, customer
+where customer.name <> '박지성' and orders.custid = customer.custid);
 
 -- 8 --
 select  count(distinct bookname) 
@@ -97,8 +104,67 @@ select name
 from customer
 where custid in (select distinct custid from orders);
 
-select name, count(orderid)
-from customer, orders
-where customer.custid=orders.custid
+select C.name, count(O.orderid)
+from customer C, orders O
+where C.custid=O.custid
 group by name
-order by count(orderid) desc;
+order by count(O.orderid) desc;
+
+select name, orderid
+from customer, orders
+where  customer.custid = orders.custid and orderid = 
+(select orders.orderid
+from orders, book
+where orders.bookid = book.bookid and book.publisher = '대한미디어');
+
+select name, orderid, publisher
+from customer, orders, book
+where customer.custid = orders.custid and book.bookid = orders.bookid and
+book.publisher = '대한미디어';
+
+SELECT YEAR(orderdate), COUNT(*) as Amount
+FROM madangdb.orders
+GROUP BY YEAR(orderdate) 
+HAVING Amount>1;
+
+-- 대한민국에 거주하는 고객의 
+select name
+from customer
+where address like '대한민국%'
+-- 위아래로 붙여 값이 나온다. 
+-- 중복된 값은 자동으로 생략하고 
+-- 생략없이 출력하고 싶으면 union all 을 적어준다.
+-- 다른 sql에선 union말고 다른 단어 사용할 수 있다.
+union 
+select name
+from customer
+where custid in(select custid from orders);
+
+
+-- 주문이 있는 고객의 이름과 주소를 나타내시오.
+select name, address
+from customer
+where exists (select * from orders where customer.custid = orders.custid);
+
+create table NewBook(
+ bookid integer primary key,
+ bookname varchar(20),
+ publisher varchar(20),
+ price integer
+ );
+ 
+ create table NewCustomer(
+ custid integer primary key,
+ name varchar(40),
+ address varchar(40),
+ phone varchar(30)
+ );
+ 
+ create table NewOrders(
+ orderid integer primary key,
+ custid integer not null ,
+ bookid integer not null,
+ saleprice integer,
+ orderdate date,
+ FOREIGN key (custid) references newcustomer(custid) on delete cascade
+ );
