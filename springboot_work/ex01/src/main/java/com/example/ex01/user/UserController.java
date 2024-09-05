be1df2db2c9b044fabc462df415c4c30c39e8847
HyeMin <whgpals641@naver.com>
2024-09-05 18:11:44 +0900

    240905

--- springboot_work/ex01/src/main/java/com/example/ex01/user/UserController.java
+++ springboot_work/ex01/src/main/java/com/example/ex01/user/UserController.java
@@ -1,6 +1,8 @@
 package com.example.ex01.user;
 
 import com.example.ex01.member.MemberRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -13,7 +15,16 @@ import java.time.LocalDateTime;
 
 @RestController
 @CrossOrigin
+@RequiredArgsConstructor
 public class UserController {
+
+//    @getBean 대신 스프링 객체 담는 통에서 자동으로 가져오는 방법
+//    @Autowired
+//    MemberRepository memberRepository;
+
+//    @RequiredArgsConstructor를 사용하는 방법
+//    위 Autowired와 같은 방법이다.
+    private final MemberRepository memberRepository;
 
     @GetMapping("hahah")
     public String aa(@RequestParam(name="name", defaultValue = "내이름") String name,
