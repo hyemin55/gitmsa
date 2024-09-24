b16f8432ee55c2f38bfeb9c2a77d2052815afc77
hyemin55 <whgpals641@naver.com>
2024-09-25 02:48:38 +0900

    240925

--- springboot_work/ex10/src/main/java/com/example/ex10/todo/Todo.java
+++ springboot_work/ex10/src/main/java/com/example/ex10/todo/Todo.java
@@ -1,11 +1,11 @@
 package com.example.ex10.todo;
 
 import com.example.ex10.user.User;
-import jakarta.persistence.CascadeType;
-import jakarta.persistence.Entity;
-import jakarta.persistence.ManyToOne;
+import jakarta.persistence.*;
 import lombok.*;
 import org.hibernate.annotations.Cascade;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.annotation.CreatedDate;
 
 import java.time.LocalDateTime;
 
@@ -15,12 +15,19 @@ import java.time.LocalDateTime;
 @Getter
 @Setter
 @ToString
+@Builder
 public class Todo {
 
-
+//    숫자 자동입력
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long idx;
+//    user는 하나이고 todo는 여러개이다.
     @ManyToOne(cascade = CascadeType.ALL)
     private User user;
+//    content 데이터 길이는 1000이다.
+    @Column(length = 1000)
     private String content;
+//    현재시간 입력
+    @CreatedDate
     private LocalDateTime Date;
 }
